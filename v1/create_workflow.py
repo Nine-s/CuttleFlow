21f9a5a4f7fdf569eba419c11893c5980ee639df
bossesimon <bossesim@hu-berlin.de>
2024-03-21 13:34:33 +0100

    Adapt split task
    - now enables other tasks to be split
    - operation, reference type to be split and split and merge tasks can be configured in file SPLIT_MERGE_TASKS.json

--- v1/create_workflow.py
+++ v1/create_workflow.py
@@ -18,14 +18,16 @@ for i in os.listdir(annotation_path):
         #print(full_path)
 annotDB = AnnotationDB(annotation_files)
 
-with open('/home/ninon/description_prototype/rnasplice-description/INFRA.json') as jsonfile:
+with open('/home/simon/GitHub/rewriting_DAWs/rnasplice-description/INFRA.json') as jsonfile:
     infra_description = json.load(jsonfile)
 
-with open('/home/ninon/description_prototype/rnasplice-description/DAW.json') as jsonfile:
+with open('/home/simon/GitHub/rewriting_DAWs/rnasplice-description/DAW.json') as jsonfile:
     daw_description = json.load(jsonfile)
 
+with open('/home/simon/GitHub/rewriting_DAWs/rnasplice-description/SPLIT_MERGE_TASKS.json') as jsonfile:
+    split_merge_tasks = json.load(jsonfile)
 
-with open('/home/ninon/description_prototype/rnasplice-description/INPUT_EVAL_REDUCED.json') as jsonfile:
+with open('/home/simon/GitHub/rewriting_DAWs/rnasplice-description/INPUT_EVAL_REDUCED.json') as jsonfile:
     input_description = json.load(jsonfile)
 
 #define objects for infra + nodes
@@ -36,7 +38,8 @@ my_infra = Infra(infra_description)
 
 # define object for DAW + task + connection
 my_DAW = DAW(daw_description, input_description, my_infra, annotDB)
-my_DAW = my_DAW.rewrite(annotationdb=annotDB, input_description=input_description)
+
+my_DAW = my_DAW.rewrite(annotationdb=annotDB, input_description=input_description, split_merge_annotation=split_merge_tasks)
 
 to_nextflow(my_DAW)
 #to_cwl(my_DAW)
